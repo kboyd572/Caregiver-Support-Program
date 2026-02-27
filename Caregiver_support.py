c38497f7cd6bfce91caa7eb0c65051786b6690bf
Kristen Boyd <kboyd72@students.kennesaw.edu>
2026-02-27 18:40:44 -0500

    Add files via upload

---
+++ Caregiver_support.py
@@ -0,0 +1,56 @@
+# Caregiver App Support
+# Program implementations
+    # Location
+    # Ask about their well-being
+    # Where to find support
+    # How to cope (hobbies, habits, forums)
+    # Community (local groups)
+    # Daily quote
+    # Caregiver quiz
+
+# ***** RUBRIC GUIDELINES:  ********
+# ______________________________________________
+# Code Functionality:
+# Code functions and meets all assignment criteria. No detectable logic errors.
+
+# In Code Comments:
+# Comments are complete and well thought out. They can be used to fully follow the logic used in the programs
+
+# Code Design:
+# Code design is efficient and well thought out. Student makes good use of functions, decisions, and loops for reusable code bits
+
+# User Experience:
+# User Experience is easy and self-evident. UI is well designed and appealing. Software use is intuitive.
+
+# Error Handling:
+# Program handles all foreseeable errors. Errors are dealt with in a way that allows the program to continue to run and allows the user to correct the issue.
+
+# CMS (GitHub) Use: 
+# Changes are committed regularly and well commented. Readme file is thorough. (Verified through activity log)
+# _______________________________________________________________________________________________________________________
+
+def start ():
+    print ("Hello, ")
+
+
+def location ():
+    loc = "Where are you located?"
+
+def well_being():
+    print ("")
+
+
+def support():
+    print ("")
+
+def cope ():
+    print ("")
+
+def community ():
+    print ("")
+
+def daily_quote():
+    print ("")
+
+def caregiver_quiz():
+    print():
