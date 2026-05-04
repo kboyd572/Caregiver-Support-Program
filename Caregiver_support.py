72bad770370dbbab751d2849cb31a305bd6cdc1b
Kristen Boyd <kboyd72@students.kennesaw.edu>
2026-05-03 23:04:06 -0400

    updated coping function;  quiz was updated with while loops to avoid errors - valides the answer within the constraints of the list. if invalid answer then loop will  make user input until correct.

--- Caregiver_support.py
+++ Caregiver_support.py
@@ -82,16 +82,11 @@ def support():
     print ("Resources for Caregivers - https://empowerline.org/resource/resources-for-caregivers/")
     print ("Peer Support Groups - http://shepherd.org/admissions/patient-experience/peer-support/groups/")
    
-
-def cope ():
-    print ("How to cope: ")
-    print ("Caring for yourself while caring for others: https://magazine.medlineplus.gov/article/caring-for-yourself-while-caring-for-others")
-    print ("Healthy Ways to Overcome Caregiver Burnout: https://www.brainandlife.org/articles/healthy-ways-overcome-caregiver-burnout")
-    # link for caregiver tips - https://www.mayoclinic.org/healthy-lifestyle/stress-management/in-depth/caregiver-stress/art-20044784
-    # link - https://www.caregiver.org/resource/emotional-side-caregiving/
-    # link - https://my.clevelandclinic.org/health/diseases/9225-caregiver-burnout
-    # link - https://www.nia.nih.gov/health/caregiving/taking-care-yourself-tips-caregivers
-
+# ********** coping *****************
+def cope():
+    print("How to cope: ")
+    print("Caring for yourself: https://magazine.medlineplus.gov/article/caring-for-yourself-while-caring-for-others")
+    print("Healthy Ways to Overcome Burnout: https://www.brainandlife.org/articles/healthy-ways-overcome-caregiver-burnout")
 
 
 # **************** quotes ***********************************
@@ -129,153 +124,161 @@ else:
     print("                      ฅ^•ﻌ•^ฅ                           ")
 print("")
 
+
+
 # **************** QUIZ ***********************************
 def caregiver_quiz():
-
-# putting quiz in loop to aviod errors - testing
-# 
-# while True:
-#         print("Question 1: What is caregiver burnout?")
-#         print("A) Feeling bored with caregiving")
-#         print("B) A state of physical, emotional, and mental exhaustion from caregiving")
-#         print("C) Taking too many breaks")
-#         print("D) Being paid too little for caregiving")
-        
-#         ques1_answer = input("Your answer: ").upper() # prints 
-#         print("You chose: " + ques1_answer)
-
-#quiz for user to test their knowledge on caregivers
+    # quiz for user to test their knowledge on caregivers
     print("CAREGIVER KNOWLEDGE QUIZ")
-print("____________________________________  ")
-print("What do know about caregivers? Answer these questions.")
-print("Input the letter of the answer of your choice: (A, B, C, or D).")
-print("")
+    print("____________________________________  ")
+    print("What do know about caregivers? Answer these questions.")
+    print("Input the letter of the answer of your choice: (A, B, C, or D).")
+    print("")
 
-#set the score variable to 0, so it can be calculated as quiz is completed
-score = 0
+    # set the score variable to 0, so it can be calculated as quiz is completed
+    score = 0
 
-# question 1
-print("Question 1: What is caregiver burnout?")
-print("A) Feeling bored with caregiving")
-print("B) A state of physical, emotional, and mental exhaustion from caregiving")
-print("C) Taking too many breaks")
-print("D) Being paid too little for caregiving")
+    # question 1
+    while True:
+        print("Question 1: What is caregiver burnout?")
+        print("A) Feeling bored with caregiving")
+        print("B) A state of physical, emotional, and mental exhaustion from caregiving")
+        print("C) Taking too many breaks")
+        print("D) Being paid too little for caregiving")
 
-ques1_answer = input("Your answer: ")
+        #will print the answer the user has chosen 
+        ques1_answer = input("Your answer: ")
+        print("You chose: " + ques1_answer)
 
-#if statement - to determince correctness of answers prints to show user their answer is correct.
-if ques1_answer == "B" or ques1_answer == "b":
-   # prints to show user their answer is correct.
-    print("Correct!")
-    
-    #if the user chooses the correct answer, then score will add increment of 1 
-    score = score + 1
-else:
-    #if incorrect, then score will not increase
-    print("Incorrect. The correct answer was B.")
-print("")
+        #valiates user input within the constraints of the list created (a-d)
+        if ques1_answer.lower() in ["a", "b", "c", "d"]:
+            if ques1_answer == "B" or ques1_answer == "b":
+                 #if correct user will be informed
+                print("Correct!")
+                score = score + 1
+            #if incorrect user will be informed           
+            else:
+                print("Incorrect. The correct answer was B.")
+            break 
+        #if input is invalid, loop will make user retry
+        else:
+            print("Input invalid! Enter A, B, C, or D")
+            print("")
 
-# question 2
-print("Question 2: Which of the following is a healthy coping strategy for caregivers?")
-print("A) Ignoring your own needs completely")
-print("B) Isolating yourself from friends and family")
-print("C) Joining a caregiver support group")
-print("D) Working longer hours without breaks")
+    # question 2
+    while True:
+        print("Question 2: Which of the following is a healthy coping strategy for caregivers?")
+        print("A) Ignoring your own needs completely")
+        print("B) Isolating yourself from friends and family")
+        print("C) Joining a caregiver support group")
+        print("D) Working longer hours without breaks")
 
-ques2_answer = input("Your answer: ")
-if ques2_answer == "C" or ques2_answer == "c":
-    print("Correct!")
-    
-    #if the user chooses the correct answer, then score will add increment of 1 
-    score = score + 1
-else:
-    #if incorrect, then score will not increase
-    print("Incorrect. The correct answer was C.")
-print("")
+        ques2_answer = input("Your answer: ")
+        print("You chose: " + ques2_answer)
 
-# question 3
-print("Question 3: Which condition is commonly associated with needing a caregiver?")
-print("A) Alzheimer's disease")
-print("B) Common cold")
-print("C) Seasonal allergies")
-print("D) Minor sprain")
+        # validate answer
+        if ques2_answer.lower() in ["a", "b", "c", "d"]:
+            if ques2_answer == "C" or ques2_answer == "c":
+                #if correct user will be informed
+                print("Correct!")
+                score = score + 1
+            #if incorrect user will be informed
+            else:
+                print("Incorrect. The correct answer was C.")
+            break
+        #if input is invalid, loop will make user retry
+        else:
+            print("Input invalid! Enter A, B, C, or D")
+            print("")
 
-ques3_answer = input("Your answer: ")
-if ques3_answer == "A" or ques3_answer == "a":
-    print("Correct!")
-    
-    #if the user chooses the correct answer, then score will add increment of 1 
-    score = score + 1
+    # question 3
+    while True:
+        print("Question 3: Which condition is commonly associated with needing a caregiver?")
+        print("A) Alzheimer's disease")
+        print("B) Common cold")
+        print("C) Seasonal allergies")
+        print("D) Minor sprain")
 
-else:
-    #if incorrect, then score will not increase
-    print("Incorrect. The correct answer was A.")
-print("")
+        ques3_answer = input("Your answer: ")
+        print("You chose: " + ques3_answer)
 
-# question 4
-print("Question 4: What is an early warning sign of caregiver stress?")
-print("A) Feeling energized every day")
-print("B) Losing interest in activities you once enjoyed")
-print("C) Sleeping too well")
-print("D) Having too much free time")
+        # validate answer
+        if ques3_answer.lower() in ["a", "b", "c", "d"]:
+            if ques3_answer == "A" or ques3_answer == "a":
+                print("Correct!")
+                score = score + 1
+            else:
+                print("Incorrect. The correct answer was A.")
+            break
+        else:
+            print("Input invalid! Enter A, B, C, or D")
+            print("")
 
-ques4_answer = input("Your answer: ")
-if ques4_answer == "B" or ques4_answer == "b":
-    print("Correct!")
-    
-    #if the user chooses the correct answer, then score will add increment of 1 
-    score = score + 1
+    # question 4
+    while True:
+        print("Question 4: What is an early warning sign of caregiver stress?")
+        print("A) Feeling energized every day")
+        print("B) Losing interest in activities you once enjoyed")
+        print("C) Sleeping too well")
+        print("D) Having too much free time")
 
-else:
-    #if incorrect, then score will not increase
-    print("Incorrect. The correct answer was B.")
-print("")
+        ques4_answer = input("Your answer: ")
+        print("You chose: " + ques4_answer)
 
-# question 5
-print("Question 5: Which resource provides FREE help connecting caregivers in Atlanta?")
-print("A) Doordash")
-#resource for caregivers in the atlanta area 
-print("B) EmpowerLine (empowerline.org)")
-print("C) Search engines")
-print("D) Github")
+        # validate answer
+        if ques4_answer.lower() in ["a", "b", "c", "d"]:
+            if ques4_answer == "B" or ques4_answer == "b":
+                print("Correct!")
+                score = score + 1
+            else:
+                print("Incorrect. The correct answer was B.")
+            break
+        else:
+            print("Input invalid! Enter A, B, C, or D")
+            print("")
 
-ques5_answer = input("Your answer: ")
-if ques5_answer == "B" or ques5_answer == "b":
-    print("Correct!")
-    
-    #if the user chooses the correct answer, then score will add increment of 1 
-    score = score + 1
-    
-else:
-    #if incorrect, then score will not increase
-    print("Incorrect. The correct answer was B.")
-print("")
+    # question 5
+    while True:
+        print("Question 5: Which resource provides FREE help connecting caregivers in Atlanta?")
+        print("A) Doordash")
+        print("B) EmpowerLine (empowerline.org)")
+        print("C) Search engines")
+        print("D) Github")
 
-# final score calculation
-print("Calculating final score.")
+        ques5_answer = input("Your answer: ")
+        print("You chose: " + ques5_answer)
 
-#calculates score out of 5, using the score variable declared above
-print("You got " + str(score) + " out of 5.")
+        # validate answer
+        if ques5_answer.lower() in ["a", "b", "c", "d"]:
+            if ques5_answer == "B" or ques5_answer == "b":
+                print("Correct!")
+                score = score + 1
+            else:
+                print("Incorrect. The correct answer was B.")
+            break
+        
+        else:
+            print("Input invalid! Enter A, B, C, or D")
+            print("")
 
-# if statements print depenting on score variable
-if score == 5:
-    #if 5, then user is an expert 
-    print("5/5 - You are an expert on caregiving!")
-    
-    print("                     ฅ₍^•⩊ •マⳊ                               ")
+    # final score calculation
+    print("Calculating final score.")
+    print("You got " + str(score) + " out of 5.")
 
-if score == 4:
-    #if 4, then user is an semi-expert
-    print("4/5 - You are a semi-caregiver expert! Just one shy!")
-    
-    #if 3, then user is knowledgable
-if score == 3:
-    print("3/5 - You're so close, and knowledgeable about caregiving!")
+    # if statements print depending on score variable
+    if score == 5:
+        print("5/5 - You are an expert on caregiving!")
+        print("                     ฅ₍^•⩊ •マⳊ                               ")
 
-#if score is < 3, then user will not see total out of 5
-if score < 3:
-    print("You are learning, and that's what caregiving is all about :) ")
-    print ("                          ദ്ദി◝ ⩊ ◜)                           ")
+    if score == 4:
+        print("4/5 - You are a semi-caregiver expert! Just one shy!")
+        
+    if score == 3:
+        print("3/5 - You're so close, and knowledgeable about caregiving!")
+
+    if score < 3:
+        print("You are learning, and that's what caregiving is all about :) ")
+        print ("                          ദ്ദി◝ ⩊ ◜)                           ")
     print(" ")
 
 
@@ -286,18 +289,8 @@ def main ():
     care_explained()
     location()
     well_being()
-
-
-#menu options 
-    # Where to find support
-    # How to cope (hobbies, habits, forums)
-    # Community (local groups)
-    # Daily quote
-    # Caregiver quiz
-    
     
 # Main menu and view for user
-
     start_menu = True
     #loop created so user can continously make choices until wanting to exit
     while start_menu == True:
